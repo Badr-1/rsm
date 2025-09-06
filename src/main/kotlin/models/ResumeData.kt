de36d704b4d044384566695b78d806126134e4f4
badr-1 <badrmohamedashraf@gmail.com>
2025-09-06 17:57:57 +0300

    fix mutability issue with ResumeData lists

--- src/main/kotlin/models/ResumeData.kt
+++ src/main/kotlin/models/ResumeData.kt
@@ -1,16 +1,15 @@
 package models
 
 import kotlinx.serialization.Serializable
-import java.util.Collections.emptyList
 
 
 @Serializable
 data class ResumeData(
     var orderedSections: List<SectionType> = SectionType.entries.filter { !it.isFixed },
     var personalInfo: PersonalInfo = PersonalInfo(),
-    var education: MutableList<Education> = emptyList(),
-    var experience: MutableList<Experience> = emptyList(),
-    var projects: MutableList<Project> = emptyList(),
+    var education: MutableList<Education> = mutableListOf(),
+    var experience: MutableList<Experience> = mutableListOf(),
+    var projects: MutableList<Project> = mutableListOf(),
     var technicalSkills: TechnicalSkills = TechnicalSkills(),
-    var certifications: MutableList<Certification> = emptyList()
+    var certifications: MutableList<Certification> = mutableListOf()
 )
